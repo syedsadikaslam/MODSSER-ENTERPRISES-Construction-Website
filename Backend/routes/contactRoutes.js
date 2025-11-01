50428f81e7b0b1ed55e14dfee3ab291c18474a88
Md Sadik <152442843+syedsadikaslam@users.noreply.github.com>
2025-11-02 04:20:32 +0530

    Delete Backend/routes directory

--- Backend/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { saveContact } = require('../controllers/contactController.js');
-
-// ✅ Route for saving contact + sending email
-router.post('/save', saveContact);
-
-module.exports = router;
