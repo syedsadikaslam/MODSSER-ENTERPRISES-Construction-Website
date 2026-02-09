0213d9ea1dc780116f704677a92fcdeb1313bce0
syedsadikaslam <mdsadiksadik464@gmail.com>
2026-02-09 11:23:00 +0530

    Updated code with new features

--- Backend/server.js
+++ Backend/server.js
@@ -44,7 +44,7 @@ app.use(cors({
     return callback(null, true);
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());
